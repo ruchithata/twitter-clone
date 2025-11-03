1da3571659d00b1c5b31d41746b51425adef289d
ruchithata <ruchitha.ta83@gmail.com>
2025-11-03 22:29:37 +0530

    signup endpoint

---
+++ backend/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose, { model, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            default: []
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            default: []
+        }
+    ],
+    profileImg: {
+        type: String,
+        default: ""
+    },
+    coverImag: {
+        type: String,
+        default: ""
+    },
+    bio: {
+        type: String,
+        default: ""
+    },
+    link: {
+        type: String,
+        default: ""
+    }
+}, {timestamps: true});
+
+
+const User = model('User', userSchema);
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './database/db.js';
+import router from './routes/auth.routes.js';
 
 const app = express();
 app.use(express.json());
+// app.use(express.urlencoded({extended: true}));  // to parse from data urlencoded.
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.get('/', (res,req)=>{
+app.get('/', (req, res)=>{
     res.send("Hello World!");
 });
+
+app.use('/api/auth', router);
 
 app.listen(PORT, async()=>{
     try{
