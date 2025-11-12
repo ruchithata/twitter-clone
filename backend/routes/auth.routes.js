23a648c9ca18d6888ccd3d16a5bca1a61824adcf
ruchithata <ruchitha.ta83@gmail.com>
2025-11-12 19:08:03 +0530

    login api

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { getuser, logout, signup } from '../controllers/auth.controller.js';
+import { getuser, login, logout, signup } from '../controllers/auth.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = Router();
 
 router.post('/signup', signup);
+router.post('/login', login);
 router.get('/getuser', protectRoute, getuser);
 router.post('/logout', logout);
 
