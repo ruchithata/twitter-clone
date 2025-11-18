cc6b6c1ca046287a61762e8e0dce417579f88659
ruchithata <ruchitha.ta83@gmail.com>
2025-11-18 22:42:09 +0530

    notification & suggestion

---
+++ backend/models/notification.model.js
@@ -0,0 +1,26 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const notificationSchema = new Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['follow', 'like']
+    },
+    read: {
+        type: Boolean,
+        default: false
+    }
+},{timestamps: true});
+
+const Notification = model('Notification', notificationSchema);
+export default Notification;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { followOrUnfollow, getUserProfile } from "../controllers/user.controller.js";
+import { followOrUnfollow, getSuggestedUsers, getUserProfile } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.get('/profile/:username', protectRoute, getUserProfile);
 userRouter.post('/follow/:id', protectRoute, followOrUnfollow);
+userRouter.get('/suggested', protectRoute, getSuggestedUsers);
 
 export default userRouter;
