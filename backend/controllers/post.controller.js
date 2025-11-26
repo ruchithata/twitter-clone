aae8ece974cafe3f24ac2b3df9dfe60a070c9838
ruchithata <ruchitha.ta83@gmail.com>
2025-11-26 10:30:10 +0530

    like api

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import {v2 as cloudinary} from "cloudinary";
@@ -82,4 +83,36 @@ const commentPost = async(req, res) => {
     }
 }
 
-export {createPost, deletePost, commentPost};
+const likeUnlikePost = async(req, res) => {
+    try{
+        const userId = req.user._id;
+        const {id:postId} = req.params;
+
+        const post = await Post.findById(postId);
+        if(!post){
+            return res.status(404).json({message: "Post not found"});
+        }
+        const userLikedPost = post.likes.includes(userId);
+        if(userLikedPost){
+            await Post.updateOne({_id: postId}, {$pull: {likes: userId}});
+            res.status(200).json({message: "Post unmliked successfully"});
+        }
+        else{
+            post.likes.push(userId);
+            await post.save();
+            const notification = new Notification({
+                from: userId,
+                to: post.user,
+                type: "like"
+            });
+            await notification.save();
+            res.status(200).json({message: "Post liked successfully"});
+        }
+    }
+    catch(err){
+        console.log("Error while liking or unliking a post", err);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export {createPost, deletePost, commentPost, likeUnlikePost};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { commentPost, createPost, deletePost } from "../controllers/post.controller.js";
+import { commentPost, createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
 
 const postRouter = Router();
 
 postRouter.post('/create', protectRoute, createPost);
 postRouter.delete('/:id', protectRoute, deletePost);
 postRouter.post('/comment/:id', protectRoute, commentPost);
+postRouter.post('/like/:id', protectRoute, likeUnlikePost);
 
 export default postRouter;
