4de834b80811e5a0a98849b1433817d897b22a0e
ruchithata <ruchitha.ta83@gmail.com>
2025-11-26 22:21:57 +0530

    get apis

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -177,4 +177,23 @@ const getFollowingPosts = async(req, res) => {
     }
 }
 
-export {createPost, deletePost, commentPost, likeUnlikePost, getAllPosts, getLikedPosts, getFollowingPosts};
+const getUserPosts = async(req, res) => {
+    try{
+        const {username} = req.params;
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        const posts = await Post.find({user: user._id}).sort({createdAt: -1})
+        .populate({path: "user", select: "-password"})
+        .populate({path: "comments.user", select: "-password"});
+
+        res.status(200).json(posts);
+    }
+    catch(err){
+        console.log("Error while getting the user's posts", err);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export {createPost, deletePost, commentPost, likeUnlikePost, getAllPosts, getLikedPosts, getFollowingPosts, getUserPosts};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { commentPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, getUserPosts, likeUnlikePost } from "../controllers/post.controller.js";
 
 const postRouter = Router();
 
 postRouter.get('/', protectRoute, getAllPosts);
 postRouter.get('/following', protectRoute, getFollowingPosts);
+postRouter.get('/user/:username', protectRoute, getUserPosts);
 postRouter.post('/create', protectRoute, createPost);
 postRouter.delete('/:id', protectRoute, deletePost);
 postRouter.post('/comment/:id', protectRoute, commentPost);
