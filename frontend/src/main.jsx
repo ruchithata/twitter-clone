8e7f5b16c5bbcd2865b3ea2b09c38b5a56a9e6c1
ruchithata <ruchitha.ta83@gmail.com>
2025-11-30 09:50:33 +0530

    frontend basic css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDom from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={new QueryClient()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+    </React.StrictMode>
 )
