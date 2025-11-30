ac265da137b553e96a33e67be859fc8cc24c59a4
ruchithata <ruchitha.ta83@gmail.com>
2025-11-30 21:50:24 +0530

    delte post added

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,7 +5,7 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const Sidebar = () => {
@@ -35,7 +35,7 @@ const Sidebar = () => {
 			toast.error("Logout failed");
 		},
 	});
-	const {data:authUser} = useQuery({queryKey: ["authUser"]});
+	const authUser = queryClient.getQueryData(["authUser"]);
 
 	return (
 		<div className='md:flex-[2_2_0] w-18 max-w-52'>
